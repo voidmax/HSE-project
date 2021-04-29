d10948eea5c16ee4ab847a4b82da0a210cb04520
voidmax <vrom272000@gmail.com>
2021-04-29 20:44:32 +0300

    kt2 results

---
+++ automated_features.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+#include <fstream>
+using namespace std;
+
+#include "tqdm.h"
+#include "markup.h"
+#include "functions.h"
+#include "table.h"
+#include "encoding.h"
+
+map<string, table> database;
+
+void extract_features(table& node, table& edge, string save_path) {
+    ofstream output;
+    output.open(save_path.c_str());
+    tqdm bar;
+    int lst = 0;
+
+    unordered_map<string, vector<pair<string, column>>> node_columns;
+    for (auto [name, col] : node.info.columns) {
+        if (name[0] == '!') {
+            continue;
+        }
+        node_columns[col["type"]].push_back({name, col});
+    } 
+    unordered_map<string, vector<pair<string, column>>> edge_columns;
+    for (auto [name, col] : edge.info.columns) {
+        if (name[0] == '!') {
+            continue;
+        }
+        edge_columns[col["type"]].push_back({name, col});
+    } 
+
+    string node_key = node_columns["key"][0].first;
+    string edge_key = edge_columns["node_key"][0].first;
+    string edge_order = edge_columns["date"][0].first;
+    cerr << "\nSorting " << node.info.name << "..." << endl;
+    node.sorting(node_key);
+    cerr << "Sorting " << edge.info.name << "..." << endl;
+    edge.sorting(edge_key);
+    cerr << "Preparing features for " << node.info.name << "..." << endl;
+    for (int i = 0; i < node.rows.size(); ++i) {
+        bar.progress(i, node.rows.size());
+        int L = lst;
+        while (L < edge.rows.size() &&
+            edge.get(L, edge_key) < node.get(i, node_key)) {
+            ++L;
+        } 
+        int R = L;
+        while (R < edge.rows.size() && 
+            edge.get(R, edge_key) == node.get(i, node_key)) {
+            ++R;
+        }
+        lst = R;
+        edge.sorting(L, R, edge_order);
+
+        feature_list features;
+
+        for (auto [name, col] : node_columns["key"]) {
+            features += create_feature(name, node.get(i, name));
+        }
+
+        for (auto [name, col] : node_columns["ordered_category"]) {
+            auto order = from_regex(col["values"]);
+            features += encode_ordered_cat(name, node.get(i, name), order);
+        }
+
+        for (auto [name, col] : node_columns["category"]) {
+            auto order = from_regex(col["values"]);
+            features += encode_one_hot(name, node.get(i, name), order);
+        }
+
+        for (auto [name, col] : node_columns["binary"]) {
+            auto order = from_regex(col["values"]);
+            features += encode_ordered_cat(name, node.get(i, name), order);
+        }
+
+        for (auto [name, col] : node_columns["binary_sequence"]) {
+            string s = node.get(i, name);
+            int sum = 0, lst, inc = 0, dec = 0;
+            for (int i = 0; i < s.size(); ++i) {
+                if (i) {
+                    inc += s[i] > s[i - 1];
+                    dec += s[i] < s[i - 1];
+                }
+                sum += s[i] - '0';
+            }
+            lst = s.back() - '0';
+            features += create_feature(name + "_sum", sum);
+            features += create_feature(name + "_lst", lst);
+            features += create_feature(name + "_inc", inc);
+            features += create_feature(name + "_dec", dec);
+        }
+
+        for (auto [name, col] : node_columns["target"]) {
+            features += create_feature("target", node.get(i, name));
+        }
+
+        for (auto [name, col] : edge_columns["category"]) {
+            auto order = from_regex(col["values"]);
+            vector<string> values;
+            for (int M = L; M < R; ++M) {
+                 values.push_back(edge.get(M, name));
+            }
+            features += encode_hist(name, values, order);
+        }
+
+        for (auto [name, col] : edge_columns["ordered_category"]) {
+            auto order = from_regex(col["values"]);
+            vector<string> values;
+            for (int M = L; M < R; ++M) {
+                 values.push_back(edge.get(M, name));
+            }
+            features += encode_hist(name, values, order);
+        }
+
+        vector<double> node_coors;
+        for (auto [name, col] : node_columns["coordinates"]) {
+            node_coors.push_back(to_double(node.get(i, name)));
+        }
+        vector<vector<double>> edge_coors;
+        for (auto [name, col] : edge_columns["coordinates"]) {
+            edge_coors.push_back({});
+            for (int M = L; M < R; ++M) {
+                if (edge.get(M, name).size()) {
+                    assert(edge.id.count(name));
+                    assert(M < edge.rows.size());
+                    edge_coors.back().push_back(to_double(edge.get(M, name)));
+                }
+            }
+        }
+        features += coor_encoding(node_coors, edge_coors);
+
+        if (i == 0) {
+            bool first_print = true;
+            for (auto f : features) {
+                if (first_print) {
+                    first_print = false;
+                } else {
+                    output << ",";
+                }
+                output << f.first;
+            }
+            output << '\n';
+        } 
+        bool first_print = true;
+        for (auto f : features) {
+            if (first_print) {
+                first_print = false;
+            } else {
+                output << ",";
+            }
+            output << f.second;
+        }
+        output << '\n';
+    }
+    bar.finish();
+    output.close();
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    debug_table_info = false;
+    vector<table_info> input = init_config();
+    for (auto cur : input) {
+        database[cur.name].info = cur;
+        read_csv(cur["!INFO"]["path"], database[cur.name]);
+    }
+
+    for (auto [table_name, edge] : database) {
+        if (edge.info["!INFO"]["category"] != "edge") {
+            continue;
+        }
+        table node;
+        for (auto [name, col] : edge.info.columns) {
+            if (name[0] == '!') {
+                continue;
+            }
+            if (col["type"] == "node_key") {
+                assert(database.count(col["path"]));
+                node = database[col["path"]];
+            }
+        }
+        assert(node.info.name.size());
+        extract_features(node, edge, "automated_features/disp_coor_" + node.info.name + ".csv");
+    }
+}   
